e366def8395c6579fe48ae3ef7898ccf3ebec546
Isira Katakumbura <isirakatakumburs@gmail.com>
2026-08-04 14:57:42 -0600

    initial update of the app with new framework. Original app found from https://github.com/lashansamwork/dating-app-project

---
+++ screens/WelcomeScreen.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export default function WelcomeScreen() {
+    return (
+        <View>
+            <Text> WelcomeScreen </Text>
+        </View>
+    );
+}
